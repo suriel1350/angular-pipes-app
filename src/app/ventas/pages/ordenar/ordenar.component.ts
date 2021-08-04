9db81461a677747062e2c5f50188c1c0f3245c2e
suriel_asael <kato_1350@hotmail.com>
2021-08-03 20:15:14 -0500

    add: add prime table

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,14 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
 
   enMayusculas: boolean = true;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman', 
+      vuela: true, 
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman', 
+      vuela: false, 
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin', 
+      vuela: false, 
+      color: Color.verde
+    },
+    {
+      nombre: 'Daredevil', 
+      vuela: false, 
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde', 
+      vuela: true, 
+      color: Color.verde
+    }
+  ]
 
   cambiar() {
     this.enMayusculas = !this.enMayusculas;
