321b9675e4f725a659b0f2265908cafd30c03007
suriel_asael <kato_1350@hotmail.com>
2021-08-03 20:45:00 -0500

    add: add parameters to own sort pipe

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -10,6 +10,7 @@ import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 export class OrdenarComponent {
 
   enMayusculas: boolean = true;
+  ordenarPor: string = ''
   heroes: Heroe[] = [
     {
       nombre: 'Superman', 
@@ -40,6 +41,10 @@ export class OrdenarComponent {
 
   cambiar() {
     this.enMayusculas = !this.enMayusculas;
+  }
+
+  cambiarOrden( valor: string ) {
+    this.ordenarPor = valor;
   }
 
 }
--- src/app/ventas/pipes/ordenar.pipe.ts
+++ src/app/ventas/pipes/ordenar.pipe.ts
@@ -6,11 +6,19 @@ import { Heroe } from '../interfaces/ventas.interfaces';
 })
 export class OrdenarPipe implements PipeTransform {
 
-  transform(heroes: Heroe[]): Heroe[] {
+  transform(heroes: Heroe[], orderPor: string = 'sin valor'): Heroe[] {
 
-    heroes = heroes.sort( (a,b) => ( a.nombre > b.nombre ) ? 1 : -1 );
+    switch( orderPor ){
 
-    return heroes;
+      case 'nombre':
+        return heroes.sort( (a,b) => ( a.nombre > b.nombre ) ? 1 : -1 );
+      case 'vuela':
+        return heroes.sort( (a,b) => ( a.vuela > b.vuela ) ? -1 : 1 );
+      case 'color':
+        return heroes.sort( (a,b) => ( a.color > b.color ) ? 1 : -1 );
+      default:
+        return heroes;
+    }
   }
 
 }
